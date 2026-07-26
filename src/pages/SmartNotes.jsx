c58e82e8eae1ff6c3a544b1705ae406808ce0e93
Hemant Prakash <208340955+hemant12578@users.noreply.github.com>
2026-07-26 07:39:50 +0530

    Fix useEffect import in SmartNotes.jsx and AnimatedCounter animation

--- src/pages/SmartNotes.jsx
+++ src/pages/SmartNotes.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useApp } from '../context/AppContext'
 import SourcesSidebar from '../components/smartnotes/SourcesSidebar'
 import AddSourceModal from '../components/smartnotes/AddSourceModal'
